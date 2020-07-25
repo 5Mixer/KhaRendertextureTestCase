eed98dcaee17d39fb92ae2cd712c049c9cdd7234
Daniel <dantheman41@live.com.au>
2020-07-25 22:15:11 +1000

    Update test case

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -1,18 +1,31 @@
 package;
 
+import kha.input.Mouse;
 import kha.Assets;
 import kha.Framebuffer;
 import kha.System;
 
 class Main {
-	static var repeats = 375; // Works when 374, fails when 375
+	static var repeats = 370; // Works when 374, fails when 375
+	// static var repeats = 375; // Works when 374, fails when 375
 	static var image:kha.Image;
 	static function render(framebuffer: Framebuffer): Void {
 		framebuffer.g2.begin();
+		trace(repeats);
 		for (x in 0...repeats) {
-			framebuffer.g2.drawImage(kha.Assets.images.sand,x*256,0);
+			// framebuffer.g2.drawScaledImage(kha.Assets.images.sand,(x%10)*10,Math.floor(x/10)*10,8,8);
+			framebuffer.g2.drawScaledImage(kha.Assets.images.sand,(x%20)*10,Math.floor(x/20)*10,8,8);
 		}
-		framebuffer.g2.drawImage(image,0,0);
+		// framebuffer.g2.drawImage(image,200,0);
+		// framebuffer.g2.drawImage(kha.Assets.images.sand,200,0);
+		
+		if (false) {
+			framebuffer.g2.end();
+			framebuffer.g2.begin(false);
+		}
+		
+		framebuffer.g2.drawImage(kha.Assets.images.water,200,0);
+		// framebuffer.g2.drawImage(kha.Assets.images.water,200,200);
 		framebuffer.g2.end();
 	}
 	
@@ -26,6 +39,10 @@ class Main {
 				g.color = kha.Color.Red;
 				g.fillRect(20,20,50,50);
 				g.end();
+				
+				Mouse.get().notify(null,null,null,function(delta) {
+					repeats += delta;
+				});
 				
 				System.notifyOnFrames(function (framebuffers) { render(framebuffers[0]); });
 			});
